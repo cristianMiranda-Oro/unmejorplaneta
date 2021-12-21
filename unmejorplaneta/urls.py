3ca8f1b621b7501869404e54a3b9d7c24068d647
cristianMiranda-Oro <cristianmirandaororstegui@gmail.com>
2021-12-21 16:04:30 -0500

    header part 2

--- unmejorplaneta/urls.py
+++ unmejorplaneta/urls.py
@@ -13,13 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
 from django.contrib import admin
 from django.urls import path
 #from django.conf.urls import url
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.views.static import serve
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-]
+
